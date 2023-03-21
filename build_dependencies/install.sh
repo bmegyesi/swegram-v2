589e94404ef26ffe16d151b919a2a8208d469a4a
rexruan <dafengyuxue@gmail.com>
2023-03-21 08:36:16 +0100

    Touch __init__.py in the tools folder

--- build_dependencies/install.sh
+++ build_dependencies/install.sh
@@ -5,6 +5,7 @@ python3 build_dependencies/install_pandoc.py
 
 # Download and install udpipe
 mkdir -p tools/udpipe
+touch tools/__init__.py
 cp -r build_dependencies/en/* tools/udpipe
 chmod +x tools/udpipe/udpipe
 
@@ -20,15 +21,13 @@ cd tools/efselab
 # Build lemmatizer
 make
 
-tar xvzf swe-pipeline-ud2.tar.gz
+tar -xvzf swe-pipeline-ud2.tar.gz
 python3 build_suc.py --skip-generate --python --n-train-fields 2
 python3 build_suc_ne.py --skip-generate --python --n-train-fields 4
 
 # Build and train the SUC-to-UD conversion model
-python3 build_udt_suc_sv.py \
-    --python --beam-size 1 --n-train-fields 4 \
-    ./udt_suc_sv train \
-    data/sv-ud-train.tab data/sv-ud-dev.tab swe-pipeline/suc-ud.bin
+python3 build_udt_suc_sv.py --python --beam-size 1 --n-train-fields 4
+./udt_suc_sv train data/sv-ud-train.tab data/sv-ud-dev.tab swe-pipeline/suc-ud.bin
 
 # Export efselab to the PYTHONPATH
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
