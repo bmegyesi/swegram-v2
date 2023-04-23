5ad89eefaacd4bba38e3711c84276b4e2c4c9b9c
rexruan <dafengyuxue@gmail.com>
2023-04-23 15:06:22 +0200

    Refactor reading conll

--- swegram_main/lib/utils.py
+++ swegram_main/lib/utils.py
@@ -3,9 +3,22 @@ import logging
 import os
 import shutil
 import tempfile
-from typing import Callable, Generator, List, Optional
+from typing import Callable, Dict, Generator, Iterator, List, Optional, TypeVar, Tuple, Union
 from hashlib import md5
 from pathlib import Path
+
+from swegram_main.data.metadata import parse_metadata
+from swegram_main.lib.converter import Converter
+from swegram_main.lib.logger import get_logger
+
+
+T = TypeVar("T", bound=List[Tuple[List[List[List[str]]], Dict[str, str]]])
+FT = TypeVar("FT", bound=Iterator[Union[Dict[str, str], str]])
+logger = get_logger(__name__)
+
+
+class MetaFormatError(Exception):
+    """Meta format error"""
 
 
 class ConllFormatError(Exception):
@@ -16,16 +29,39 @@ class AnnotationError(Exception):
     """Annotation error"""
 
 
+class FileContent:
+
+    def __init__(self, filepath: Path) -> None:
+        self.filepath = filepath
+
+    def _convert(self):
+        yield from Converter(self.filepath).parse()
+
+    def get(self) -> FT:
+        for line in self._convert():
+            if not line:
+                continue
+            metadata = parse_metadata(line)
+            if metadata is None:
+                yield line
+            else:
+                yield metadata
+
+
 def get_md5(filepath: Path) -> str:
     """Get md5 value"""
     with codecs.open(filepath, "rb") as inputfile:
         return md5(inputfile.read()).hexdigest()
 
 
-def get_conllu_md5(filepath: Path) -> None:
+def get_conll_md5(filepath: Path) -> str:
     with open(filepath, "r") as input_file:
         lines = [line for line in input_file.readlines() if not line.startswith("#")]
         return md5("\n".join(lines).encode()).hexdigest()
+
+
+def get_content_md5(content: str) -> str:
+    return md5(content).hexdigest()
 
 
 def get_size(filepath: Path) -> Optional[int]:
@@ -51,41 +87,79 @@ def read(filepath: Path) -> Generator:
             line = input_file.readline()
 
 
-def read_conllu(filepath: Path) -> List[List[List[str]]]:
-    """Read conllu text"""
-    with codecs.open(filepath, mode="r", encoding="utf-8") as input_file:
-        paragraphs, sentences, sentence = [], [], []
-        newline = 0
-        line = input_file.readline()
-        while line:
-            if line == "\n":
-                newline += 1
-            elif line.startswith("#"):
-                pass
+def _initialize_conllu_reading(file_content: FT) -> Tuple[Union[None, Dict[str, str]], Union[str, Dict[str, str]]]:
+    component = next(file_content)
+    while True:
+        # Skip blank lines in the beginning of the file content
+        if isinstance(component, str):
+            if not component.strip():
+                component = next(file_content)
             else:
-                if not newline:
-                    sentence.append(line)
-                elif newline == 2:
-                    if sentences:
-                        if sentence:
-                            sentences.append(sentence)
-                        paragraphs.append(sentences)
-                        sentences, sentence = [], [line]
-                        newline = 0
-                elif newline == 1:
-                    if sentence:
-                        sentences.append(sentence)
-                        sentence = [line]
-                        newline = 0
-                else:
-                    raise ConllFormatError(f"Too many blank lines, max 2 newlines, but got {newline}")
-            line = input_file.readline()
+                return None, component
+        elif isinstance(component, dict):
+            return component, next(file_content)
+        else:
+            raise MetaFormatError(f"Invalid format, got {type(component)}:{component}")
+    
 
-        if sentence:
+def read_conll_file(input_path: Path) -> T:
+    """Read conllu text"""
+    texts: T = []
+    paragraphs, sentences, sentence, = [], [], []
+    file_content = FileContent(input_path).get()
+
+    def _append_paragraph() -> None:
+        if sentences and sentence:
+            sentences.append(sentence)
+        elif sentence:
             sentences.append(sentence)
         if sentences:
             paragraphs.append(sentences)
-        return paragraphs
+
+    def _append_text(meta: Dict[str, str]) -> None:
+        _append_paragraph()
+        if paragraphs:
+            texts.append((paragraphs, meta))
+        elif meta:
+            logger.warning(f"Medata data {meta}: text is empty")
+
+    try:
+        meta, component = _initialize_conllu_reading(file_content)
+        while True:
+            newline = 0
+            while True:
+                if isinstance(component, str):
+                    if component == "\n":
+                        newline += 1
+                    elif component.startswith("#"):
+                        pass
+                    else:
+                        if not newline:
+                            sentence.append(component)
+                        elif newline == 2:
+                            _append_paragraph()
+                            sentence = [component]
+                            newline = 0
+                        elif newline == 1:
+                            if sentence:
+                                sentences.append(sentence)
+                                sentences, sentence = [], [component]
+                                newline = 0
+                        else:
+                            raise ConllFormatError(f"Too many blank lines, max 2 newlines , but got {newline}")
+                elif isinstance(component, dict):
+                    _append_text(meta)
+                    meta = component
+                    paragraphs = []
+                    break
+                else:
+                    raise MetaFormatError(f"Invalid format, got {type(component)}:{component}")
+                component = next(file_content)
+            component = next(file_content)
+
+    except StopIteration:
+        _append_text(meta)
+    return texts
 
 
 def cut(
