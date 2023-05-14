7972300b5b85f2ef850f1be4d57187d44d43ef0a
rexruan <dafengyuxue@gmail.com>
2023-05-14 09:55:52 +0200

    Add metadata in the output

--- swegram_main/handler/visualization.py
+++ swegram_main/handler/visualization.py
@@ -19,6 +19,10 @@ class Visualization:
     ) -> None:
         self.language = language
         self.input_path = input_path
+        include_labels = include_tags or []
+        exclude_labels = exclude_tags or []
+        self.labels = f"Include metadata: {' '.join(include_labels)}\n" if include_labels else ""
+        self.labels += f"Exclude metadata: {' '.join(exclude_labels)}\n" if exclude_labels else ""
         self.corpus: Corpus = load(input_path, language, include_tags, exclude_tags)
         self.outdir = output_dir or Path(os.getcwd())
 
@@ -61,31 +65,44 @@ class Visualization:
             self.save(data)
         elif save_as == "json":
             with open(self.outfile_name, "w") as output_file:
-                json_object = json.dumps(self.serialize_data(data), indent=4)
+                data["metadata"] = self.get_json_header()
+                json_object = json.dumps(self.serialize_json_data(data), indent=4)
                 output_file.write(json_object)
 
-    def serialize_data(self, data: Any) -> str:
+    def serialize_json_data(self, data: Any) -> str:
         if isinstance(data, OrderedDict):
             for key, value in data.items():
                 if isinstance(value, Feature):
                     data[key] = value.json
                 elif isinstance(value, list):
-                    data[key] = self.serialize_data(value)
+                    data[key] = self.serialize_json_data(value)
         elif isinstance(data, list):
             for index, instance in enumerate(data):
-                data[index] = self.serialize_data(instance)
+                data[index] = self.serialize_json_data(instance)
         return data
-                
+
     def append_in_text(self, content: str) -> None:
         with open(self.outfile_name, "a+") as output_file:
             output_file.write(f"{content}\n")
 
-    def get_header(self):
+    def get_header(self) -> str:
         return "Swegram statistic\n" \
                f"Time: {str(datetime.now())}\n" \
                f"Language: {self.language}\n" \
+               f"Labels: {self.labels}" if self.labels else "" \
                f"Units: {self.units}\n" \
                f"Aspects: {self.aspects}\n"
+
+    def get_json_header(self) -> Dict[str, str]:
+        headers = {
+            "Time": str(datetime.now()),
+            "Language": self.language,
+            "Units": self.units,
+            "Aspects": self.aspects
+        }
+        if self.labels:
+            headers.update({"Labels": self.labels})
+        return headers
 
     def filter_instance(
         self, instance, include_features: List[str], exclude_features: List[str]
