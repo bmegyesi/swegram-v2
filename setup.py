756c318ce2729c29458bdae9875577814f9b7e4c
rexruan <dafengyuxue@gmail.com>
2023-06-02 00:57:54 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ from setuptools import setup, find_packages
 from typing import List
 
 
-def read(fname: str) -> str:
-    with open(os.path.join(os.path.dirname(__file__), fname)) as input_file:
-        return input_file.read()
+Base = Path(__file__).parent.resolve()
 
 
 def get_requirements() -> List[str]:
@@ -18,15 +16,57 @@ def get_requirements() -> List[str]:
 
 setup(
     name="swegram",
-    version="1.0.0.dev0",
+    version="1.0.0.dev5",
     # author="",
     # author_email="",
     description="CLI library for Swegram",
-    # long_description=read("README.md"),
+    # long_description=(Base / "README.md").read_text(encoding="utf-8"),
     packages=find_packages(exclude=["tools*", "test*"]),
-    license=read("LICENSE"),
+    license=(Base / "LICENSE").read_text(encoding="utf-8"),
     # url="url",
     install_requires=get_requirements(),
+    package_data={
+        "swegram_main.statistics.kelly": ["kelly.en", "kelly.sv", "wpm.sv"],
+        "build_dependencies": ["install.sh"],
+        "build_dependencies.en.en": ["english-ud-2.0-170801.udpipe"],
+        "build_dependencies.en.histnorm": ["bnc.mono.en.freqs"],
+        "build_dependencies.en.histnorm.WordNetDictionary": [
+            "adj.exc", "adv.exc", "data.adj", "data.adv", "data.noun", "data.verb", "noun.exc", "verb.exc"
+        ],
+        "build_dependencies.sv": ["swe-pipeline-ud2.tar.gz"],
+        "build_dependencies.sv.HistNorm.scripts": [
+            "compute_editdistance.perl",
+            "extractWeights.perl",
+            "findEdits.perl",
+            "normalise_levenshtein.perl",
+            "normalise_levenshtein_elevtexter.perl",
+            "setWeightsAndThreshold.sh",
+            "set_threshold.perl"
+        ],
+        "build_dependencies.sv.HistNorm.resources.swedish.levenshtein": [
+            "fullList.txt",
+            "saldo-total_wordforms.txt",
+            "sv-threshold.txt",
+            "sv-weights.txt",
+            "swedish.corp",
+            "swedish.dev.txt",
+            "swedish.dic",
+            "swedish.hs-sv.dev.hs",
+            "swedish.hs-sv.dev.hssv",
+            "swedish.hs-sv.dev.sv",
+            "swedish.hs-sv.test.hs",
+            "swedish.hs-sv.test.hssv",
+            "swedish.hs-sv.test.sv",
+            "swedish.hs-sv.train.hs",
+            "swedish.hs-sv.train.hssv",
+            "swedish.hs-sv.train.sv",
+            "swedish.train.txt",
+            "threshold.swedish.elevtexter.txt",
+            "threshold.swedish.txt",
+            "weights.swedish.txt"
+        ]
+    },
+    include_package_data=True,
     entry_points={
         "console_scripts": [
             "swegram=swegram_main.handler.cli:main"
